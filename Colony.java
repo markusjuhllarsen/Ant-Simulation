f68467fc2bd879b149e92f5eccdd7ac0ac54947f
Markus Juhl Larsen <markl19@student.sdu.dk>
2024-07-13 22:28:11 +0200

    Commenting cleanup

--- Colony.java
+++ Colony.java
@@ -4,28 +4,28 @@
  */
 public class Colony extends Node{
 
-	//Stored sugar in colony
+	// Stored sugar in colony
 	private int consumptionStock;
 	
-	//Creates colony with no stored sugar and no transportable sugar
+	// Creates colony with no stored sugar and no transportable sugar
 	public Colony(){
-		//Calling the constructor without arguments from the superclass Node
+		// Calling the constructor without arguments from the superclass Node
 		super();
 		consumptionStock = 0;
 	}
 	
-	//Checks whether colony has stored sugar
+	// Checks whether colony has stored sugar
 	public boolean hasStock(){
 		return consumptionStock > 0;
 	}
 	
-	//Simulates ant eating sugar
-	//Precondition: there is sugar in this colony's stock
+	// Simulates ant eating sugar
+	// Precondition: there is sugar in this colony's stock
 	public void consume(){
 		consumptionStock -= 1;
 	}
 	
-	//Adding carried sugar to stored sugar
+	// Adding carried sugar to stored sugar
 	public void topUp(int carriedSugar){
 		consumptionStock += carriedSugar;
 	}
